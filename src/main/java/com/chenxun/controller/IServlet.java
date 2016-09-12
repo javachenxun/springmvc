619d62bdf5ad26c7bbfd9fc4359666295d4ec0c3
chenxun985323117@126.com <chenxun985323117@126.com>
2016-09-13 00:19:41 +0800

    init

---
+++ src/main/java/com/chenxun/controller/IServlet.java
@@ -0,0 +1,49 @@
+package com.chenxun.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Servlet implementation class IServlet
+ */
+public class IServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	private final Log  log = LogFactory.getLog(this.getClass());
+   
+    public IServlet() {
+        super();
+        log.info("====IServlet()===");
+    }
+
+
+	public void init() throws ServletException {
+		log.info("====init(ServletConfig config)===");
+	}
+
+
+	public ServletConfig getServletConfig() {
+		log.info("====getServletConfig()===");
+		return null;
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.info("====doGet===");
+		String aa = request.getParameter("aa");
+		response.getWriter().write(aa);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.info("====doPost===");
+		this.doGet(request, response);
+	}
+
+}
